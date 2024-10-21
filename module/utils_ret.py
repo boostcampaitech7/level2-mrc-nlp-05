90222cdc2b089884862445f2e3bd17b48184e8d1
Mujae <phs514525@nate.com>
2024-10-21 02:10:27 +0000

    fix bug in-batch code

--- module/utils_ret.py
+++ module/utils_ret.py
@@ -29,18 +29,19 @@ def build_dense_embedding(p_encoder, q_encoder, contexts, tokenizer):
     """
     p_embedding = []
     p_encoder.eval()
-    dense_embedding_path = os.path.join("/data/ephemeral/data/", 
-    "dense_embedding.bin")
+    dense_embedding_path = os.path.join("/data/ephemeral/data/", "dense_embedding.bin")
     q_encoder_path = os.path.join("/data/ephemeral/data/", "q_encoder.bin")
 
     for passage in tqdm(contexts, desc="Building dense embeddings"):
-        passage_inputs = tokenizer(passage,
-        return_tensors="pt",
-        truncation=True,
-        padding="max_length").to("cuda")
+        passage_inputs = tokenizer(
+            passage,
+            return_tensors="pt",
+            truncation=True,
+            padding="max_length"
+        ).to("cuda")
 
         with torch.no_grad():
-            p_emb = p_encoder(**passage_inputs)  # pooled_output 사용
+            p_emb = p_encoder(**passage_inputs)  # CLS
         p_embedding.append(p_emb.cpu().numpy())
 
     with open(dense_embedding_path, "wb") as file:
@@ -54,7 +55,10 @@ def prepare_in_batch_negative(config, contexts, tokenizer, model_args, data_args
     Prepare in-batch negative samples for training.
     """
     dataset = load_from_disk(data_args.train_dataset_name)["train"]
-    tokenized_contexts = [doc.split() for doc in contexts]
+    dataset1 = load_from_disk('/data/ephemeral/data/train_dataset')["train"]['context']
+    dataset2 = load_from_disk('/data/ephemeral/data/train_dataset')["validation"]['context']
+    dataset1.extend(dataset2)
+    tokenized_contexts = [doc.split() for doc in dataset1]
     bm25 = BM25Okapi(tokenized_contexts)
     q_input_ids, q_attention_mask, q_token_type_ids = [], [], []
     p_input_ids, p_attention_mask, p_token_type_ids = [], [], []
@@ -63,30 +67,36 @@ def prepare_in_batch_negative(config, contexts, tokenizer, model_args, data_args
         bert = True
 
     for i, context in enumerate(dataset["context"]):
-        q_encodings = tokenizer(dataset["question"][i],
-        truncation=True,
-        padding="max_length",
-        return_tensors="pt")
-        q_input_ids.append(q_encodings["input_ids"].tolist())
-        q_attention_mask.append(q_encodings["attention_mask"].tolist())
+        q_inputs = tokenizer(
+            dataset["question"][i],
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt"
+        )
+        q_input_ids.append(q_inputs["input_ids"].tolist())
+        q_attention_mask.append(q_inputs["attention_mask"].tolist())
 
         neg_contexts = sample_negatives(dataset["question"][i],
             data_args.num_neg, 
             context, 
-            contexts,
+            dataset1,
             bm25,
         )
-        p_encodings = tokenizer([context] + neg_contexts,
-        truncation=True,
-        padding="max_length",
-        return_tensors="pt")
-        p_input_ids.append(p_encodings["input_ids"].tolist())
-        p_attention_mask.append(p_encodings["attention_mask"].tolist())
-        if bert:
-            q_token_type_ids.append(q_encodings["token_type_ids"].tolist())
-            p_token_type_ids.append(p_encodings["token_type_ids"].tolist())
 
-    size = q_encodings["input_ids"].size(-1)
+        p_inputs = tokenizer(
+            [context] + neg_contexts,
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt"
+        )
+
+        p_input_ids.append(p_inputs["input_ids"].tolist())
+        p_attention_mask.append(p_inputs["attention_mask"].tolist())
+        if bert:
+            q_token_type_ids.append(q_inputs["token_type_ids"].tolist())
+            p_token_type_ids.append(p_inputs["token_type_ids"].tolist())
+
+    size = p_inputs["input_ids"].size(-1)
     dataset = create_tensor_dataset(
         bert,
         data_args.num_neg,
@@ -112,7 +122,7 @@ def sample_negatives(question, num_neg, context, contexts, bm25):
     neg_contexts = []
 
     for idx in ranked_idxs:
-        if contexts[idx] != context:
+        if contexts[idx][:10] != context[:10]:
             neg_contexts.append(contexts[idx])
         if len(neg_contexts) == num_neg:
             break
@@ -136,9 +146,9 @@ def create_tensor_dataset(
         return TensorDataset(
             torch.tensor(q_input_ids),
             torch.tensor(q_attention_mask),
-            torch.tensor(q_token_type_ids),
             torch.tensor(p_input_ids).view(-1, num_neg + 1, size),
             torch.tensor(p_attention_mask).view(-1, num_neg + 1, size),
+            torch.tensor(q_token_type_ids),
             torch.tensor(p_token_type_ids).view(-1, num_neg + 1, size),
         )
 
